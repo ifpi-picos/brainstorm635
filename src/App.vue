41ef09371e32953afdaba0aa4728572664723afa
edmarques11 <edmarquessantosdelima@gmail.com>
2020-11-30 17:16:32 -0300

    Tela de inicio do brainstorm

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,8 @@
 </template>
 
 <script>
-
 import NavBar from '@/components/navbar.vue'
 import Footer from '@/components/footer.vue'
-
 export default {
   name: 'default',
   components: {
@@ -23,21 +21,18 @@ export default {
     return {
     }
   }
-
   /* mounted () {
     console.log(this.$firebase)
   } */
 }
 </script>
 <style>
-
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50 !important;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
--- src/views/startBrainstorm.vue
+++ src/views/startBrainstorm.vue
@@ -1,14 +1,73 @@
 <template>
-  <b-container fluid>
-    <b-row align-v="center">
-      <b-col class="align-items-center justify-content-center ml-auto mr-auto" md="7">
-        <b-card class="text-center pr-5 pl-5 pb-5 pt-0">
-          <b-card-text>
-            Some quick example text to build on the card title and make up the bulk
-            of the card's content.
-          </b-card-text>
+  <b-container class="mt-5">
+    <b-row class="mb-5">
+      <b-col>
+        <b-card>
+          <b-card-body class="d-flex justify-content-between">
+            <span><i class="fas fa-file-signature fa-lg"></i>Descrição</span>
+            <span
+              ><i class="fas fa-circle-notch fa-lg mr-auto ml-auto"></i
+              >Round</span
+            >
+            <span><i class="far fa-clock fa-lg"></i>Tempo restante</span>
+          </b-card-body>
+        </b-card>
+      </b-col>
+    </b-row>
 
-          <b-button href="#" variant="primary">Go somewhere</b-button>
+    <b-row align-v="center">
+      <b-col
+        class="align-items-center justify-content-center ml-auto mr-auto mb-2 h-100"
+      >
+        <b-card class="text-center pr-0 pl-0 pb-0 pt-0">
+          <b-card-body>
+            <b-form-group>
+              <label for="ideia1">Ideia 1</label>
+              <b-form-textarea
+                id="ideia1"
+                placeholder="Digite sua ideia..."
+                class="entradaTexto"
+              ></b-form-textarea>
+            </b-form-group>
+            <div class="cor"></div>
+          </b-card-body>
+        </b-card>
+      </b-col>
+
+      <b-col
+        class="align-items-center justify-content-center ml-auto mr-auto mb-2 h-100"
+      >
+        <b-card class="text-center pr-0 pl-0 pb-0 pt-0">
+          <b-card-body>
+            <b-form-group>
+              <label for="ideia2">Ideia 2</label>
+              <b-form-textarea
+                id="ideia2"
+                placeholder="Digite sua ideia..."
+                class="entradaTexto"
+              ></b-form-textarea>
+            </b-form-group>
+            <div class="cor"></div>
+          </b-card-body>
+        </b-card>
+      </b-col>
+
+      <b-col
+        class="align-items-center justify-content-center ml-auto mr-auto mb-2 h-100"
+      >
+        <b-card class="text-center pr-0 pl-0 pb-0 pt-0">
+          <b-card-body>
+            <b-form-group>
+              <label for="ideia3">Ideia 3</label>
+              <b-form-textarea
+                id="ideia3"
+                placeholder="Digite sua ideia..."
+                class="entradaTexto"
+                wrap="hard"
+              ></b-form-textarea>
+            </b-form-group>
+            <div class="cor"></div>
+          </b-card-body>
         </b-card>
       </b-col>
     </b-row>
@@ -20,10 +79,43 @@ export default {}
 </script>
 
 <style lang="css" scoped>
-.cartao{
+/* .cartao{
   display: flex !important;
   flex-direction: column;
   align-content: center;
   align-items: center;
+} */
+
+.entradaTexto {
+  display: flex;
+  height: 200px !important;
+  border: none !important;
+  flex-wrap: wrap;
+}
+
+.entradaTexto::-webkit-scrollbar {
+  width: 4px;
+}
+
+.entradaTexto::-webkit-scrollbar-track {
+  background: #cecece;
+  border-radius: 3px;
+}
+
+.entradaTexto::-webkit-scrollbar-thumb {
+  background: rgb(112, 112, 231);
+  border-radius: 3px;
+}
+
+.entradaTexto::-webkit-scrollbar-thumb:hover {
+  background: blue;
+}
+
+.cor {
+  display: block;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background: blue;
 }
 </style>
