6cbb224a39db2ac8743ec19ad17ce5638fe60d0e
Rafaelmaiden <rafaelmaggot64@gmail.com>
2021-02-05 16:54:51 -0300

    Added button to print brainstorm

--- src/views/printBrainstorm.vue
+++ src/views/printBrainstorm.vue
@@ -1,59 +1,71 @@
 <template>
-  <b-container fluid class="container-ideas">
-    <b-col md="12">
-      <h5 class="page-tittle mb-3 mt-3">Ideas Report</h5>
-    </b-col>
-    <b-col xs="12" sm="12" md="12" class="text-left mb-3">
-      <span>
-        <span class="icone icone-padd">
-          <i class="fa fa-file-signature fa-lg"></i>
-        </span>
-        {{ description }}
-      </span>
-    </b-col>
-    <b-row class="mb-4 text-center" v-for="(roundy, i) in ideasPerRound" :key="i">
-      <b-col md="12" class="mb-1">
-        <h5 class="round">Round {{ i+1 }} </h5>
-      </b-col>
+  <div>
+    <Loader v-if="loading"> </Loader>
+    <b-container v-if="!loading" fluid class="container-ideas">
       <b-col md="12">
-        <b-row v-for="(values, key) in roundy" :key="key">
-          <b-col class="mb-4 pl-1 pr-1" md="4" v-for="(value, index) in values" :key="index">
-            <div class="postit">
-              <h5 class="text-center"> <b> Idea #{{ index+1 }} </b> </h5>
-              <b-card-text>
-              <p style="font-size: 1rem">
-                {{ value }}
-              </p>
-              </b-card-text>
-              <!-- <p
-                class="text-muted"
-                style="position: absolute; bottom: -10px">
-                Editado por:
-                <b>{{ guestNames[index] }}</b>
-              </p> -->
-            </div>
-          </b-col>
-        </b-row>
+        <h5 class="page-tittle mb-3 mt-3">Ideas Report</h5>
       </b-col>
-    </b-row>
-    <b-row class="mr-auto">
-      <b-col>
+      <b-col xs="12" sm="12" md="12" class="text-left mb-3">
         <span>
-          <b class="date" > Brainstorm date: </b> {{ date }}
+          <span class="icone icone-padd">
+            <i class="fa fa-file-signature fa-lg"></i>
+          </span>
+          {{ description }}
         </span>
       </b-col>
-    </b-row>
-  </b-container>
+      <b-row class="mb-4 text-center" v-for="(roundy, i) in ideasPerRound" :key="i">
+        <b-col md="12" class="mb-1">
+          <h5 class="round">Round {{ i+1 }} </h5>
+        </b-col>
+        <b-col md="12">
+          <b-row v-for="(values, key) in roundy" :key="key">
+            <b-col class="mb-4 pl-1 pr-1" md="4" v-for="(value, index) in values" :key="index">
+              <div class="postit">
+                <h5 class="text-center"> <b> Idea #{{ index+1 }} </b> </h5>
+                <b-card-text>
+                <p style="font-size: 1rem">
+                  {{ value }}
+                </p>
+                </b-card-text>
+                <!-- <p
+                  class="text-muted"
+                  style="position: absolute; bottom: -10px">
+                  Editado por:
+                  <b>{{ guestNames[index] }}</b>
+                </p> -->
+              </div>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row >
+        <b-col md="9" class="ml-0 pl-0">
+          <span class="text-date">
+            <b class="date" > Brainstorm date: </b> <i> {{ date }} </i>
+          </span>
+        </b-col>
+        <b-col md="3" class="text-right pr-0">
+          <b-button
+            onClick="window.print()"
+            variant="outline-info">
+            Print brainstorm
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import Loader from '../components/loader'
 export default {
+  components: { Loader },
   data () {
     return {
+      loading: true,
       brainstormId: this.$route.params.id,
       datasOFBrainstorm: {},
       date: '',
-      round: [],
       rounds: [],
       ideasPerRound: [],
       description: '',
@@ -79,7 +91,7 @@ export default {
             this.date = doc.data().timestamp.toDate()
             this.description = doc.data().description
 
-            for (let i = 1; i < this.rounds.length + 1; i++) {
+            for (let i = 1; i < this.rounds.length; i++) {
               const index = 'round' + i
               /* console.log(index) */
               try {
@@ -91,9 +103,8 @@ export default {
                   .doc(index)
                   .get()
                   .then(doc => {
-                    this.round = doc.data()
-                    this.ideasPerRound.push(this.round)
-                    /* console.log(this.ideasPerRound) */
+                    this.ideasPerRound.push(doc.data())
+                    console.log(this.ideasPerRound)
                   })
               } catch (error) {
                 console.error(error)
@@ -115,6 +126,7 @@ export default {
                 console.error(error)
               } */
             }
+            this.loading = false
           })
       } catch (error) {
         console.error(error)
@@ -141,15 +153,17 @@ $colorDark: darken($color, 10%) transparent;
   min-height: 15em;
   max-height: 15em;
   min-width: 13em;
+  border: 1px solid $color;
 }
 
 .postit:after {
+  background: $color;
   content: "";
   position: absolute;
   bottom: -1.5em;
-  left: 0;
+  left: -1px;
   right: 1.5em;
-  border-width: 1em;
+  border-width: 0.8em;
   border-style: solid;
   border-color: $color;
 }
@@ -167,6 +181,8 @@ $colorDark: darken($color, 10%) transparent;
 .container-ideas {
   max-width: 80% !important;
   min-width: 80% !important;
+  padding-left: 0 !important;
+  padding-right: 0 !important;
 }
 
 h5, p, span {
@@ -181,5 +197,9 @@ h5, p, span {
 
 .date {
   color: #138496;
+}
+
+.text-date {
+  font-size: 18px;
 }
 </style>
